95b1cd173a0b703fbd7f136e5840937b79b0ebd0
Andr27 <Andr27chet04@gmail.com>
2026-02-14 15:46:37 +1000

    add comments

--- backend/Portal/apps/users/urls.py
+++ backend/Portal/apps/users/urls.py
@@ -5,7 +5,10 @@ from .views import RegistrationAPIView, MeAPIView, ConfirmEmailAPIView
 
 
 urlpatterns = [
-    path('register/', RegistrationAPIView.as_view()),
-    path('confirm-email/<uuid:token>/', ConfirmEmailAPIView.as_view()),
-    path('me/', MeAPIView.as_view()),
+
+    path('register/', RegistrationAPIView.as_view()), # post
+    path('confirm-email/<uuid:token>/', ConfirmEmailAPIView.as_view()), #get
+    path('me/', MeAPIView.as_view()), #get
 ]
+
+
